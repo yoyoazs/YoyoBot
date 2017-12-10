4cd7116a8c83abec51ee708d05ee69c09bd6440e
yoyoazs <34071396+yoyoazs@users.noreply.github.com>
2017-12-10 02:05:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on("ready", () => {
   console.log("Je suis pres a l'utilisation!");
 })
 
-bot.login("Mzg3NjY5MTMzMzA0NTI4ODk3.DQxl8A.7gLdOdbSRkK-CPserFh8XkJGmjY");
+bot.login(process.env.TOKEN);
 
 bot.on("guildMemberAdd", member =>{
 	member.guild.channels.find("name","welcome").channel.send(message,`Bienvenue a ${member.user.username} sur le serveur !`)
