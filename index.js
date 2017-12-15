355967ec7f29f944067eba54a1c3f0cc627e9332
yoyoazs <34071396+yoyoazs@users.noreply.github.com>
2017-12-15 23:40:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on('error', (e) => console.error(e));
 bot.on('warn', (e) => console.warn(e));
 // bot.on('debug', (e) => console.info(e));
 
-bot.login(config.token);
+bot.login(bot.login(process.env.TOKEN));
 
 function getCmdFunction(cmd) {
     const COMMANDS = {
